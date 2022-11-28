aa8c75234dceb69c20819396220582c310911da5
SandraBarth <s2010238005@fhooe.at>
2022-11-28 18:22:39 +0000

    Topic 10 - Session based Auth

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import {genres } from './genresData';
+import Genre from './genreModel';
 
 const router = express.Router(); 
 
 //get genres
-router.get('/', (req, res) => {
-    res.json(genres);
+router.get('/', async (req, res) => {
+    const genres = await Genre.find();
+    res.status(200).json(genres);
 });
 
 export default router;
